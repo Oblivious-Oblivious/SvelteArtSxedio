b1f26b83659bcbee1ef584b4e5e22de79f99a73b
Oblivious <me262thanos@gmail.com>
2021-09-04 03:16:10 +0300

    Added tailwind and implemented the base layout of the SPA.

---
+++ client/src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<main>
+    <slot></slot>
+</main>
+
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+
+    * {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
