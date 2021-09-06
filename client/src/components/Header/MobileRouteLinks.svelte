db1f40a9bf3840639e2dab92dc486c6c11f4d7ca
Oblivious <me262thanos@gmail.com>
2021-09-06 05:36:55 +0300

    Added a custom column navbar with specialised css.

---
+++ client/src/components/Header/MobileRouteLinks.svelte
@@ -0,0 +1,70 @@
+<ul>
+    <li on:click>
+        <a href="/">Home</a>
+    </li>
+    <li on:click>
+        <a href="/gallery">Gallery</a>
+    </li>
+    <li on:click>
+        <a href="/shop">Shop</a>
+    </li>
+    <li on:click>
+        <a href="/about">About</a>
+    </li>
+</ul>
+
+<style>
+    ul {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    a {
+        font-weight: 300;
+        display: flex;
+        position: relative;
+        text-align: center;
+        width: fit-content;
+        margin: 0 auto;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        padding: 0.2em 0;
+        /* padding: 12px 16px; */
+    }
+
+    /* Fade in */
+    a::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 1px;
+        background-color: var(--underline-color);
+        opacity: 0;
+        transition: opacity 400ms, transform 400ms;
+    }
+
+    a:hover::after,
+    a:focus::after {
+        opacity: 1;
+        transform: translate3d(0, 0.2em, 0);
+    }
+
+    /* Slide in */
+    li a {
+        overflow: hidden;
+    }
+    
+    li a::after {
+        opacity: 0;
+        transform: translate3d(-100%, 0, 0);
+    }
+
+    li a:hover::after,
+    li a:focus::after{
+        transform: translate3d(0, 0, 0);
+    }
+</style>
