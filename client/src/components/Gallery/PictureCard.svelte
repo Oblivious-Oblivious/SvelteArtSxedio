2d3a05956ae7869b6c0bc4326d87f7caaa91b65a
Oblivious <me262thanos@gmail.com>
2021-09-07 23:56:09 +0300

    Added all layout picture cards in columns with responsiveness on different sizes.

---
+++ client/src/components/Gallery/PictureCard.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let image_details;
+</script>
+
+<a href={image_details[0]} draggable="false">
+    <img src={image_details[0]} alt="rose" draggable="false">
+    <h3>{image_details[1]}</h3>
+</a>
+
+<style>
+    img {
+        cursor: pointer;
+    }
+
+    h3 {
+        color: var(--text-color);
+        font-weight: 400;
+        line-height: 1.5;
+        margin: 1rem 0 1rem 0;
+        font-size: 1rem;
+    }
+
+    a {
+        border-bottom: none;
+        display: block;
+        width: 100%;
+        margin: 0 0 2rem 0;
+        text-align: center;
+        border-radius: 4px;
+        box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.25),
+            0 0 0.5em 0 var(--border-color);
+        background-color: rgba(255, 255, 255, 0.075);
+        cursor: pointer;
+        outline: 0;
+        overflow: hidden;
+        transition: all 200ms ease;
+    }
+
+    a img {
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+    }
+
+    a:hover {
+        background-color: rgba(255, 255, 255, 0.25);
+        box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.25),
+            0 0 0.5em 0 var(--yellow);
+    }
+</style>
--- client/src/routes/gallery.svelte
+++ client/src/routes/gallery.svelte
@@ -2,15 +2,70 @@
     import { onMount } from "svelte";
     import { underline_value } from "../stores/underline_value";
 
+    import Column from "../components/Gallery/Column.svelte";
+    
     onMount(() => $underline_value = 2);
+    
+    let image_list1 = [
+        ["img/tall/Rose.JPG", "Rose"],
+        ["img/wide/River Boat.JPG", "River Boat"],
+        ["img/wide/Fruit.JPG", "Fruit"],
+        ["img/tall/Poppies.JPG", "Poppies"],
+        ["img/wide/Leaves.JPG", "Leaves"]
+    ];
+    let image_list2 = [
+        ["img/ultra-wide/details/Pumkin detail1.JPG", "Pumkin"],
+        ["img/wide/Waves.JPG", "Waves"],
+        ["img/wide/Rocks.JPG", "Rocks"],
+        ["img/tall/River.JPG", "River"],
+        ["img/tall/Salt Shaker.JPG", "Salt Shacker"]
+    ];
+    let image_list3 = [
+        ["img/tall/Ver Meer.JPG", "Ver Meer"],
+        ["img/tall/Dali.JPG", "Dali"],
+        ["img/wide/Peaches.JPG", "Peaches"],
+        ["img/tall/Pencil.JPG", "Pencil"],
+        ["img/ultra-wide/Naxos.JPG", "Naxos"]
+    ];
 </script>
 
-<h1>Gallery</h1>
+<div class="image-grid">
+    <div class="image-col"><Column image_list={image_list1}/></div>
+    <div class="image-col"><Column image_list={image_list2}/></div>
+    <div class="image-col"><Column image_list={image_list3}/></div>
+</div>
 
 <style>
-    h1 {
-        text-align: center;
-        font-size: 2.5rem;
-        padding: 40rem 20rem;
+    .image-grid {
+        padding-top: 4.5rem;
+        display: flex;
+        justify-content: center;
+    }
+
+    .image-col {
+        display: flex;
+        flex-direction: column;
+        width: 24%;
+        padding: 0 1rem;
+    }
+
+    @media (max-width: 1024px) {
+        .image-grid {
+            padding-top: 5rem;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .image-col {
+            width: 50%;
+            margin: 0 auto;
+            padding: 0 1rem;
+        }
+    }
+
+    @media (max-width: 769px) {
+        .image-col {
+            width: 75%;
+        }
     }
 </style>
