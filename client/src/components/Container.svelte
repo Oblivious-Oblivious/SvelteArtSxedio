ba149f98d98ec4e62ed996e748fef8d673f47320
Oblivious <me262thanos@gmail.com>
2021-09-08 06:47:11 +0300

    Implemented the about page.
    TODO Fix blockquote overflow.

---
+++ client/src/components/Container.svelte
@@ -0,0 +1,24 @@
+<div class="container">
+    <div class="content">
+        <slot></slot>
+    </div>
+</div>
+
+<style>
+    .container {
+        font-family: var(--font);
+        margin: 0 auto;
+        background: var(--body-bg-color);
+        box-shadow: 5px 5px 15px var(--shadow-color);
+        border-radius: 10px;
+    }
+
+    .content {
+        color: var(--text-color);
+        font-size: 1.25rem;
+        font-weight: 300;
+        text-align: center;
+        padding: 1.25rem;
+        margin: 2rem 0;
+    }
+</style>
--- client/src/routes/about.svelte
+++ client/src/routes/about.svelte
@@ -2,15 +2,41 @@
     import { onMount } from "svelte";
     import { underline_value } from "../stores/underline_value";
 
+    import BlockQuote from "../components/BlockQuote.svelte";
+    import Container from "../components/Container.svelte";
+
     onMount(() => $underline_value = 4);
 </script>
 
-<h1>About</h1>
+<div class="about-container">
+    <BlockQuote cite="&mdash;Vang 2019">
+        On catefor up to the fanar, turn off
+        engine and car tsouls down nicely.
+    </BlockQuote>
+    
+    <img src="./img/about.JPG" alt="about" draggable="false"/>
+    
+    <Container>
+        Super painter, sallonica arch dropout, because he alredy
+        know these things, close to becoming OTE employee, has
+        education as topograph at metsovo village.
+        <br><br><br>
+        Ah, he also makes karavaks.
+    </Container>
+</div>
 
 <style>
-    h1 {
-        text-align: center;
-        font-size: 2.5rem;
-        padding: 40rem 20rem;
+    .about-container {
+        width: 75%;
+        margin: 0 auto;
+    }
+
+    img {
+        width: 75%;
+        margin: 0 auto;
+        margin-top: 1rem;
+        margin-bottom: 2rem;
+        box-shadow: 5px 5px 15px var(--shadow-color);
+        border-radius: 10px;
     }
 </style>
