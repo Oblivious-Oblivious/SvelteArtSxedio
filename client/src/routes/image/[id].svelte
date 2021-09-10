e2643fa27249d397c0f3ef86e132db27895ec34e
Oblivious <me262thanos@gmail.com>
2021-09-11 01:19:13 +0300

    Added a dynamic route for images.

---
+++ client/src/routes/image/[id].svelte
@@ -0,0 +1,25 @@
+<script context="module">
+    // import image_list from "../../stores/images_api";
+
+    export async function load({page}) {
+        const id = page.params.id;
+        const image = `id: ${id}`;
+
+        return {
+            props: {image}
+        };
+    }
+</script>
+
+<script>
+    export let image;
+</script>
+
+<h2>{image}</h2>
+
+<style>
+    h2 {
+        font-size: 2rem;
+        margin: 4rem auto;
+    }
+</style>
