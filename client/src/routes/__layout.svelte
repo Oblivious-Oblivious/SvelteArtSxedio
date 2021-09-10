7de3fc45891188444f3652d2d61f977619986690
Oblivious <me262thanos@gmail.com>
2021-09-10 19:42:12 +0300

    Fixed overscroll color behaviour.

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -19,8 +19,21 @@
     export let refresh;
 </script>
 
+<svelte:head>
+    <style>
+        body {
+            font-family: var(--font);
+            font-feature-settings: "lnum"; /* Line-up number symbols */
+
+            color: var(--text-color);
+            background-color: var(--body-bg-color);
+            background-size: cover;
+            background-position: top center;
+        }
+    </style>
+</svelte:head>
+
 <!-- TODO Add smooth loading -->
-<!-- TODO Add same color overscroll behaviour -->
 
 <main>
     <Header/>
@@ -31,19 +44,3 @@
 
     <Footer/>
 </main>
-
-<style>
-    /* @import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"); */
-
-    main {
-        /* font-family: Raleway; */
-        font-family: var(--font);
-        font-feature-settings: "lnum"; /* Line-up number symbols */
-
-        color: var(--text-color);
-        background-color: var(--body-bg-color);
-        overflow: hidden;
-        background-size: cover;
-        background-position: top center;
-    }
-</style>
