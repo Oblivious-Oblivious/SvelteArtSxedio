726c474ee706af9cbc862b686d215216cdef6517
Oblivious <me262thanos@gmail.com>
2021-09-12 22:49:15 +0300

    Added the blur animation as the smooth loading transition.

--- client/src/components/SmoothLoading.svelte
+++ client/src/components/SmoothLoading.svelte
@@ -1,14 +1,11 @@
 <script>
-    import { fly } from "svelte/transition";
+    import { blur } from "../animations/blur";
 
     export let refresh = "";
 </script>
 
 {#key refresh}
-    <div
-        in:fly="{{ y: -50, duration: 250, delay: 300 }}"
-        out:fly="{{ y: -50, duration: 250 }}"
-    >
+    <div transition:blur>
         <slot/>
     </div>
 {/key}
