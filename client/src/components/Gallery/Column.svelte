e8130036ff486ecff899116e3bf90c32f3ade16f
Oblivious <me262thanos@gmail.com>
2021-09-13 22:25:37 +0300

    Changed name to GalleryCard.
    Will be adding WorkCard in the future.

--- client/src/components/Gallery/Column.svelte
+++ client/src/components/Gallery/Column.svelte
@@ -1,11 +1,11 @@
 <script>
-    import PictureCard from "./PictureCard.svelte";
+    import GalleryGard from "./GalleryCard.svelte";
 
     export let image_list;
 </script>
 
 <div>
     {#each image_list as image}
-        <PictureCard image_details={image}/>
+        <GalleryGard image_details={image}/>
     {/each}
 </div>
