bd8627cc3a31e10a838cf0cb285f4b6c8c24c20e
Oblivious <me262thanos@gmail.com>
2021-09-13 05:30:43 +0300

    Added a first version of a simple lightbox effect.
    TODO Need simplify css and smooth out transitions.

--- client/src/components/Gallery/PictureCard.svelte
+++ client/src/components/Gallery/PictureCard.svelte
@@ -1,12 +1,29 @@
 <script>
+    import { fade } from "svelte/transition";
+
     export let image_details;
+
+    let is_visible = false;
+    const toggle_visibility = () =>
+        is_visible = !is_visible;
 </script>
 
-<!-- TODO Add poptrox effect -->
-<a href={`image/${image_details[0]}`} draggable="false">
-    <img src={image_details[1]} alt="imgsrc" draggable="false">
-    <h3>{image_details[2]}</h3>
-</a>
+<div on:click={toggle_visibility} class="thumbnail" draggable="false">
+    <img src={image_details[0]} alt="imgsrc" draggable="false">
+    <h3>{image_details[1]}</h3>
+</div>
+
+{#if is_visible}
+    <div
+        transition:fade
+        on:click={toggle_visibility}
+        class="lightbox lightbox-img lightbox-visible"
+    >
+        <div class="lightbox-placeholder" role="dialog">
+            <img src={image_details[0]} alt="imgsrc" draggable="false">
+        </div>
+    </div>
+{/if}
 
 <style>
     img {
@@ -22,7 +39,7 @@
         font-size: 1rem;
     }
 
-    a {
+    .thumbnail {
         border-bottom: none;
         display: block;
         width: 100%;
@@ -38,14 +55,76 @@
         transition: all 200ms ease;
     }
 
-    a img {
+    .thumbnail img {
         border-top-left-radius: 4px;
         border-top-right-radius: 4px;
     }
 
-    a:hover {
+    .thumbnail:hover {
         background-color: rgba(255, 255, 255, 0.25);
         box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.25),
             0 0 0.5em 0 var(--yellow);
+    }
+
+    .lightbox {
+        position: fixed;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        
+        box-sizing: content-box;
+        background: rgba(0, 0, 0, 0.75);
+        overflow: hidden;
+
+        opacity: 0.01;
+        transition: opacity 400ms ease;
+        z-index: 1000;
+        will-change: opacity;
+    }
+
+    .lightbox-visible {
+        opacity: 1;
+    }
+
+    .lightbox-placeholder {
+        max-width: 100%;
+        transform: scale(0.9);
+        transition: transform 400ms ease;
+        z-index: 1;
+        will-change: transform;
+    }
+
+    .lightbox-placeholder img:first-child:last-child {
+        display: block;
+        position: absolute;
+
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        margin: auto;
+
+        width: auto;
+        height: auto;
+        max-width: 87.5%;
+        max-height: 87.5%;
+
+        border-radius: 4px;
+        box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.25),
+            0 0 0.5em 0 var(--white-color);
+    }
+
+    .lightbox-img .lightbox-placeholder {
+        width: 100%;
+        height: 100%;
+        pointer-events: none;
+    }
+
+    .lightbox-visible .lightbox-placeholder {
+        transform: scale(1);
     }
 </style>
