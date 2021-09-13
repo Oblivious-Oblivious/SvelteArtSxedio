1e46f89a13e28dd74366c8349b375acbd511a42a
Oblivious <me262thanos@gmail.com>
2021-09-13 23:29:33 +0300

    Extracted lightbox functionality from gallery card.

--- client/src/components/Gallery/GalleryCard.svelte
+++ client/src/components/Gallery/GalleryCard.svelte
@@ -1,28 +1,24 @@
 <script>
-    import { fade } from "svelte/transition";
+    import Lightbox from "./Lightbox.svelte";
 
-    export let image_details;
+    export let image;
+    let [image_source, image_caption] = image;
 
-    let is_visible = false;
+    let lightbox_is_visible = false;
     const toggle_visibility = () =>
-        is_visible = !is_visible;
+        lightbox_is_visible = !lightbox_is_visible;
 </script>
 
 <div on:click={toggle_visibility} class="thumbnail" draggable="false">
-    <img src={image_details[0]} alt="imgsrc" draggable="false">
-    <h3>{image_details[1]}</h3>
+    <img src={image_source} alt="imgsrc" draggable="false">
+    <h3>{image_caption}</h3>
 </div>
 
-{#if is_visible}
-    <div
-        transition:fade
+{#if lightbox_is_visible}
+    <Lightbox
         on:click={toggle_visibility}
-        class="lightbox lightbox-img lightbox-visible"
-    >
-        <div class="lightbox-placeholder" role="dialog">
-            <img src={image_details[0]} alt="imgsrc" draggable="false">
-        </div>
-    </div>
+        {image_source}
+    />
 {/if}
 
 <style>
@@ -64,67 +60,5 @@
         background-color: rgba(255, 255, 255, 0.25);
         box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.25),
             0 0 0.5em 0 var(--yellow);
-    }
-
-    .lightbox {
-        position: fixed;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100vh;
-        
-        box-sizing: content-box;
-        background: rgba(0, 0, 0, 0.75);
-        overflow: hidden;
-
-        opacity: 0.01;
-        transition: opacity 400ms ease;
-        z-index: 1000;
-        will-change: opacity;
-    }
-
-    .lightbox-visible {
-        opacity: 1;
-    }
-
-    .lightbox-placeholder {
-        max-width: 100%;
-        transform: scale(0.9);
-        transition: transform 400ms ease;
-        z-index: 1;
-        will-change: transform;
-    }
-
-    .lightbox-placeholder img:first-child:last-child {
-        display: block;
-        position: absolute;
-
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        margin: auto;
-
-        width: auto;
-        height: auto;
-        max-width: 87.5%;
-        max-height: 87.5%;
-
-        border-radius: 4px;
-        box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.25),
-            0 0 0.5em 0 var(--white-color);
-    }
-
-    .lightbox-img .lightbox-placeholder {
-        width: 100%;
-        height: 100%;
-        pointer-events: none;
-    }
-
-    .lightbox-visible .lightbox-placeholder {
-        transform: scale(1);
     }
 </style>
